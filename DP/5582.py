e5d4d6cf2b4e311c4b38956a6baa77476b2b296c
PSW <tkddnjs0211@gmail.com>
2022-05-08 16:38:06 +0900

    edit : 1. sys.stdin.readline 2. finding max value

--- DP/5582.py
+++ DP/5582.py
@@ -1,17 +1,17 @@
 # https://www.acmicpc.net/problem/5582
-
-s1 = input()
-s2 = input()
+import sys
+input = sys.stdin.readline
+s1 = input().strip()
+s2 = input().strip()
 l1 = len(s1)
 l2 = len(s2)
 
+ans = 0
 dp = [[0 for _ in range(l2+1)] for _ in range(l1+1)]
 for i in range(1, l1+1):
     for j in range(1, l2+1):
         if s1[i-1] == s2[j-1]:
             dp[i][j] = dp[i-1][j-1] + 1
+            ans = max(ans, dp[i][j])
 
-ans = 0
-for i in range(len(dp)):
-    ans = max(ans, max(dp[i]))
 print(ans)
